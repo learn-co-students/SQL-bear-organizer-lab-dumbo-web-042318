32cb97ca6e3af08b397c63178159bd4bed24a6fe
snkoski <shawnnkoski@gmail.com>
2018-04-28 19:01:24 -0400

    First SQLite lab, tracking bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 35, "M", "Brown", "Nice", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 16, "M", "Brown", "Mean", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 65, "F", "Black", "Mean", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 42, "M", "Black", "Mean", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 18, "F", "Brown", "Nice", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 38, "M", "Black", "Mean", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 9, "F", "Brown", "Nice", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, 19, "M", "Brown", "Mean", 0);
