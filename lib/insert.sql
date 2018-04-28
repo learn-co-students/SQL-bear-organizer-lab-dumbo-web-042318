744501c405fd2d5d22212704148e49c8699abd2b
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-04-28 15:09:37 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Mr. Chocolate", 11, "m", "brown", "fine", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Rowdy", 12, "m", "brown", "rowdy-ish", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Tabitha", 13, "f", "black", "happy", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Seargeant Brown", 14, "m", "brown/black", "gruff", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Melissa", 15, "f", "brown", "easy going", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Grinch", 16, "m", "brown", "mean", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Wendy", 17, "f", "brown", "Flame Grilled", "true");
-INSERT INTO bears (age, gender, color, temperament, alive)
-VALUES (12, "m", "brown", "fine", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 11, "m", "brown", "fine", "true"),
+("Rowdy", 12, "m", "brown", "rowdy-ish", "true"),
+("Tabitha", 13, "f", "black", "happy", "true"),
+("Seargeant Brown", 14, "m", "brown/black", "gruff", "true"),
+("Melissa", 15, "f", "brown", "easy going", "true"),
+("Grinch", 16, "m", "brown", "mean", "true"),
+("Wendy", 17, "f", "brown", "Flame Grilled", "true"),
+(null, 12, "m", "brown", "fine", "true");
