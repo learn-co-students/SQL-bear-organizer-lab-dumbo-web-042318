52bea0cda0420e10dae7bc6189919b896f476e55
github username <github email address>
2018-04-28 20:51:58 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,8 @@
 INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
-INSERT INTO bears VALUES (2, "Rowdy", 10, "M", "black", "intense", 1)
-INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1),
-INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "nice", 1),
-INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "brown", "friendly", 0),
-INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-INSERT INTO bears VALUES (6,"Gaucho", 2, "M", "black", "shy", 1),
-INSERT INTO bears VALUES (7,"Wendy", 6, "F", "dark brown", "friendly", 1),
+INSERT INTO bears VALUES (2, "Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "nice", 1);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "brown", "friendly", 0);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Gaucho", 2, "M", "black", "shy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 6, "F", "dark brown", "friendly", 1);
 INSERT INTO bears VALUES (8,null, 20, "M", "brown", "aggressive", 0);
