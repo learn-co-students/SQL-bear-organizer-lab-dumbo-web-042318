2e128b5fd776ce13ead4ee4420032508591e4efa
Daniel Shimon <danielzshimon@gmail.com>
2018-04-29 20:29:58 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+insert into bears (name) values ("Mr. Chocolate
+");
+insert into bears (name) values ("Rowdy
+");
+insert into bears (name) values ("Tabitha");
+insert into bears (name) values ("Sergeant Brown
+");
+insert into bears (name) values ("Melissa");
+insert into bears (name) values ("Grinch");
+insert into bears (name) values ("Wendy");
+insert into bears (name) values (null);
