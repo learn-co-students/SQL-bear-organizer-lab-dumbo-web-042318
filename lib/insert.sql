df1cfc7ae8fdffb286c0a238f19832635ba54d67
github username <github email address>
2018-04-30 17:00:36 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+/*Populate table */
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 13, "f", "Brown", "Docile", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "m", "Brown", "Aggressive", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "f", "Black", "Docile", "false");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 25, "M", "Brown", "Aggressive", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "f", "Light-Brown", "Unkown", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 38, "f", "Brown", "Docile", "true");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 13, "f", "Brown", "Docile", "true");
+
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (13, "f", "Brown", "Very Aggressive", "true");
